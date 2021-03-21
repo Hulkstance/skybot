a5f8e1a04bd1d4ef3f075cb905d7bf8884855632
Hulkstance <33224919+Hulkstance@users.noreply.github.com>
2021-03-21 23:01:47 +0200

    The theme is set up

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VexModule } from '../@vex/vex.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CustomLayoutModule } from './custom-layout/custom-layout.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    // Vex
+    VexModule,
+    CustomLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
