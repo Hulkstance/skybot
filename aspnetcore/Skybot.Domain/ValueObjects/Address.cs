9bc13968143e23018096059e81ac282c12224a3e
Hulkstance <33224919+Hulkstance@users.noreply.github.com>
2021-03-26 02:44:06 +0200

    Initial

---
+++ aspnetcore/Skybot.Domain/ValueObjects/Address.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Skybot.Domain.ValueObjects
+{
+    public class Address : ValueObject
+    {
+        public string Street { get; }
+        public string City { get; }
+        public string State { get; }
+        public string Country { get; }
+        public string ZipCode { get; }
+
+        public Address() { }
+
+        public Address(string street, string city, string state, string country, string zipcode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipcode;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Street;
+            yield return City;
+            yield return State;
+            yield return Country;
+            yield return ZipCode;
+        }
+    }
+}
